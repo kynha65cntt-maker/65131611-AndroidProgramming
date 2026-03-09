be6a2ee8db3723920444a3ed4367cdd5df7640f8
kynha65cntt-maker <ky.nha.65cntt@ntu.edu.vn>
2026-03-09 07:16:26 +0700

    sua Bai2_1

--- BaiTH2_12/app/src/main/java/till/edu/baith2_1/MainActivity.kt
+++ BaiTH2_12/app/src/main/java/till/edu/baith2_1/MainActivity.kt
@@ -17,17 +17,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            BaiTH2_1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+setContent {
+    BaiTH2_1Theme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
         }
     }
+}
+}
 }
 
 @Composable
@@ -38,10 +39,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BaiTH2_1Theme {
-        Greeting("Android")
-    }
-}
---
+++ BaiTH2_12/app/src/test/java/till/edu/baith2_1/MainActivityTest.kt
@@ -0,0 +1,14 @@
+package till.edu.baith2_1
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainActivityTest {
+    @org.junit.jupiter.api.Test
+    fun greeting() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun greetingPreview() {
+    }
+
+}
