2dc293cec765456270550965d857c0a30ee653ac
kynha65cntt-maker <ky.nha.65cntt@ntu.edu.vn>
2026-03-23 07:52:25 +0700

    Bai6_OnClickXuLyTong
    Onclic_XuLyTong

--- AppCong2/app/src/main/java/till/edu/appcong/MainActivity.java
+++ AppCong2/app/src/main/java/till/edu/appcong/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+//Bai6_OnclickXuLyCong
 public class MainActivity extends AppCompatActivity {
 
     @Override
