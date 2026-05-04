e6b33866448922acdc800f03b45c776c7d9c6d89
An Ky <nhaky241205.nvtroi2023@gmail.com>
2026-05-04 09:31:13 +0700

    Firebase commit 2
    Uppdage firebase

--- QuanLychitieu/app/src/main/java/anky/ntu/edu/quanlychitieu/MainActivity.java
+++ QuanLychitieu/app/src/main/java/anky/ntu/edu/quanlychitieu/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World! My name is Nguyen Huu An Ky");
+
     }
 }
