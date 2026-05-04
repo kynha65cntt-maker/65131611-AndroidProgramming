b880aa85fd482cf23dc3a4c0c24d0465918b4c74
An Ky <nhaky241205.nvtroi2023@gmail.com>
2026-05-04 09:56:44 +0700

    commit 3
    cap nhat firebase

--- QuanLychitieu/app/src/main/java/anky/ntu/edu/quanlychitieu/MainActivity.java
+++ QuanLychitieu/app/src/main/java/anky/ntu/edu/quanlychitieu/MainActivity.java
@@ -1,6 +1,10 @@
 package anky.ntu.edu.quanlychitieu;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,23 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World! My name is Nguyen Huu An Ky");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                // Xu ly du lieu
+                Toast.makeText(getBaseContext(),"Value is : " +value, Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(getBaseContext(),"Failed to read value." , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
